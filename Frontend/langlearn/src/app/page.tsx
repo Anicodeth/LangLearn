d9dbaabcd7a0d52666facd102c6512b9bb81fc26
Ananya <afmtrendaid@gmail.com>
2024-03-05 22:13:53 +0300

    initial

--- Frontend/langlearn/src/app/page.tsx
+++ Frontend/langlearn/src/app/page.tsx
@@ -1,6 +1,31 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
-    
+
+    <div>
+
+    <Card className = "w-1/5">
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+</div>
+
+
   );
 }
