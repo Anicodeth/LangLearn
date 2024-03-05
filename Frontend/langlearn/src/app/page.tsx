4f4a6b0b65a84acd700bca0e8b4a1b2eea0c10d2
Ananya <afmtrendaid@gmail.com>
2024-03-06 00:02:31 +0300

    initial

--- Frontend/langlearn/src/app/page.tsx
+++ Frontend/langlearn/src/app/page.tsx
@@ -1,32 +1,25 @@
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
-
-    <div className = "h-screen flex items-center justify-center">
-
-    <Card className = "w-1/5 h-fit">
-  <CardHeader>
-    <CardTitle>Lang Learn</CardTitle>
-    <CardDescription>The AI language Teacher</CardDescription>
-  </CardHeader>
-  <CardFooter className = "flex-col items-start">
-    <p>Join us</p>
-    <Button>Sign Up</Button>
-
-  </CardFooter>
-
-</Card>
-</div>
-
-
+    <div className="h-screen flex items-center justify-center">
+      <Card className="w-1/5 h-fit">
+        <CardHeader>
+          <CardTitle>Lang Learn</CardTitle>
+          <CardDescription>The AI language Teacher</CardDescription>
+        </CardHeader>
+        <CardFooter className="flex-col items-start">
+          <p>Join us</p>
+          <Button>Sign Up</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
