57c45bf967c578f87ed3a5947441c252d0f17be9
Ananya <afmtrendaid@gmail.com>
2024-03-13 16:02:30 +0300

    Changes

--- Frontend/langlearn/src/app/auth/signup/page.tsx
+++ Frontend/langlearn/src/app/auth/signup/page.tsx
@@ -8,6 +8,7 @@ import {
   IconBrandGoogle,
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
+import { Button } from "@/components/ui/button";
 
 export default function SignUp() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -52,13 +53,18 @@ export default function SignUp() {
           />
         </LabelInputContainer>
 
-        <button
+        <Button>
+          Sign up &rarr;
+          <BottomGradient />
+        </Button>
+
+        {/* <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
           Sign up &rarr;
           <BottomGradient />
-        </button>
+        </button> */}
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
