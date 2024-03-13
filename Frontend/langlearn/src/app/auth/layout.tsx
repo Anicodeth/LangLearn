8d287c0c66854cd84abd8e51c335c703ffa91d4d
Ananya <afmtrendaid@gmail.com>
2024-03-13 22:02:16 +0300

    Side Image

--- Frontend/langlearn/src/app/auth/layout.tsx
+++ Frontend/langlearn/src/app/auth/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
@@ -5,8 +7,13 @@ export default function AuthLayout({
 }>) {
   return (
     <div className="h-screen border-0 flex items-center justify-center">
-      <div className="border-0 w-full h-full flex justify-center items-center side-image">
-        Image section
+      <div className="border-0 w-full h-full flex justify-center items-center ">
+        <Image
+        src = "/vr.png"
+          alt="Example Image"
+          width={500} // Set width and height for the image
+          height={300}
+        />
       </div>
       <div className="border-0 w-full h-full flex justify-center items-center bg-mainlighter">
         {" "}
