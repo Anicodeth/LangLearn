bc6615f5ec4cd71d2d593c7e97225a79837d1286
Ananya <afmtrendaid@gmail.com>
2024-03-13 21:23:00 +0300

    Side Image

--- Frontend/langlearn/src/app/auth/layout.tsx
+++ Frontend/langlearn/src/app/auth/layout.tsx
@@ -4,11 +4,11 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="border-2 w-full h-full flex justify-center items-center side-image">
+    <div className="h-screen border-0 flex items-center justify-center">
+      <div className="border-0 w-full h-full flex justify-center items-center side-image">
         Image section
       </div>
-      <div className="border-0 w-full h-full flex justify-center items-center bg-primary">
+      <div className="border-0 w-full h-full flex justify-center items-center bg-mainlighter">
         {" "}
         {children}
       </div>
