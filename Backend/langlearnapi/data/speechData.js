60968c98a54a1fc66eda440d213b854e3f63f028
Ananya <afmtrendaid@gmail.com>
2024-03-15 19:30:55 +0300

    changes

---
+++ Backend/langlearnapi/data/speechData.js
@@ -0,0 +1,15 @@
+const openai = require("../configs/openAiConfig");
+
+async function speech(text) {
+  const speechFile = path.resolve("./speech.mp3");
+  const mp3 = await openai.audio.speech.create({
+    model: "tts-1",
+    voice: "alloy",
+    input: text,
+  });
+
+  console.log(speechFile);
+  const buffer = Buffer.from(await mp3.arrayBuffer());
+  await fs.promises.writeFile(speechFile, buffer);
+  return speechFile;
+}
