3c5be138b338461f59578a25a920ec5a5021ccc3
Ananya <afmtrendaid@gmail.com>
2024-03-15 17:41:10 +0300

    changes

--- Backend/langlearnapi/configs/geminiAiConfig.js
+++ Backend/langlearnapi/configs/geminiAiConfig.js
@@ -1,7 +1,3 @@
-
-// node --version # Should be >= 18
-// npm install @google/generative-ai
-
 const {
   GoogleGenerativeAI,
   HarmCategory,
@@ -11,7 +7,7 @@ const {
 const MODEL_NAME = "gemini-1.0-pro";
 const API_KEY = "AIzaSyBgwIEIym1m5ea6tY6xhCWeEGCjW2KfmgY";
 
-async function runChat() {
+
   const genAI = new GoogleGenerativeAI(API_KEY);
   const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
@@ -45,20 +41,13 @@ async function runChat() {
     generationConfig,
     safetySettings,
     history: [
-      {
-        role: "user",
-        parts: [{ text: "make a json student date"}],
-      },
-      {
-        role: "model",
-        parts: [{ text: "```json\n{\n  \"name\": \"John Doe\",\n  \"age\": 20,\n  \"gender\": \"male\",\n  \"address\": {\n    \"street\": \"123 Main Street\",\n    \"city\": \"Anytown\",\n    \"state\": \"CA\",\n    \"zip\": \"12345\"\n  },\n  \"phone\": \"555-123-4567\",\n  \"email\": \"johndoe@example.com\",\n  \"courses\": [\n    {\n      \"name\": \"English\",\n      \"grade\": \"A\"\n    },\n    {\n      \"name\": \"Math\",\n      \"grade\": \"B\"\n    },\n    {\n      \"name\": \"Science\",\n      \"grade\": \"C\"\n    }\n  ]\n}\n```"}],
-      },
+
     ],
   });
 
   const result = await chat.sendMessage("What is the age of John?");
   const response = result.response;
   console.log(response.text());
-}
+
 
 runChat();
