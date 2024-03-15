8e326bdfbe9139f5f649e0a15e100b44afade22b
Ananya <afmtrendaid@gmail.com>
2024-03-15 17:41:18 +0300

    changes

--- Backend/langlearnapi/configs/geminiAiConfig.js
+++ Backend/langlearnapi/configs/geminiAiConfig.js
@@ -7,47 +7,41 @@ const {
 const MODEL_NAME = "gemini-1.0-pro";
 const API_KEY = "AIzaSyBgwIEIym1m5ea6tY6xhCWeEGCjW2KfmgY";
 
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+const generationConfig = {
+  temperature: 0.9,
+  topK: 1,
+  topP: 1,
+  maxOutputTokens: 2048,
+};
 
-  const generationConfig = {
-    temperature: 0.9,
-    topK: 1,
-    topP: 1,
-    maxOutputTokens: 2048,
-  };
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+];
 
-  const safetySettings = [
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-  ];
+const chat = model.startChat({
+  generationConfig,
+  safetySettings,
+  history: [],
+});
 
-  const chat = model.startChat({
-    generationConfig,
-    safetySettings,
-    history: [
-
-    ],
-  });
-
-  const result = await chat.sendMessage("What is the age of John?");
-  const response = result.response;
-  console.log(response.text());
-
-
-runChat();
+const result = await chat.sendMessage("What is the age of John?");
+const response = result.response;
+console.log(response.text());
