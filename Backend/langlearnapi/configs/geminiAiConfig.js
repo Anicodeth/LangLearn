56c70eff2017163d172789971a7659f226d5777b
Ananya <afmtrendaid@gmail.com>
2024-03-15 17:50:52 +0300

    changes

--- Backend/langlearnapi/configs/geminiAiConfig.js
+++ Backend/langlearnapi/configs/geminiAiConfig.js
@@ -45,3 +45,9 @@ const Gemini = model.startChat({
 
 
 module.exports = Gemini;
+
+
+// Usage
+// const result = await Gemini.sendMessage("What is the age of John?");
+// const response = result.response;
+// console.log(response.text());
