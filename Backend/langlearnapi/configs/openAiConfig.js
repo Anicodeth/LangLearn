ca2e7c69e74a802f3c9169c73942f0a8113e4ed7
Ananya <afmtrendaid@gmail.com>
2024-03-15 19:03:11 +0300

    changes

--- Backend/langlearnapi/configs/openAiConfig.js
+++ Backend/langlearnapi/configs/openAiConfig.js
@@ -1,29 +1,33 @@
-const  fs  = require("fs");
-const  path =  require("path");
-const OpenAI =  require("openai");
+const fs = require("fs");
+const path = require("path");
+const OpenAI = require("openai");
 
 const openai = new OpenAI({
   apiKey: "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",
 });
 
-const speechFile = path.resolve("./speech.mp3");
-
-async function main() {
+async function speech() {
+  const speechFile = path.resolve("./speech.mp3");
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
     voice: "alloy",
     input: "Hi Seleshi how are you doing today?",
-    
   });
+
   console.log(speechFile);
   const buffer = Buffer.from(await mp3.arrayBuffer());
   await fs.promises.writeFile(speechFile, buffer);
 }
-// main();
 
- const completion = await openai.chat.completions.create({
-   messages: [{ role: "user", content: "what is the capital of ethiopia" }],
-   model: "gpt-3.5-turbo",
- });
+speech();
 
- console.log(completion.choices[0]);
+async function text() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "what is the capital of ethiopia" }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+text();
