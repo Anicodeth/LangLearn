ba11b019a6e1bba8fa02eb163d567331474fa9f4
Ananya <afmtrendaid@gmail.com>
2024-03-17 15:20:35 +0300

    changes

--- Backend/langlearnapi/data/textData.js
+++ Backend/langlearnapi/data/textData.js
@@ -1,6 +1,6 @@
 const openai = require("../configs/openAiConfig");
 
-async function text(prompt) {
+async function textOpenAi(prompt) {
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: prompt }],
     model: "gpt-3.5-turbo",
