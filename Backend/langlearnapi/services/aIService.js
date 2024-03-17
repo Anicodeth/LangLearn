abab1a269d930159ee1b3e329d14399566ccd233
Ananya <afmtrendaid@gmail.com>
2024-03-17 16:07:16 +0300

    changes

--- Backend/langlearnapi/services/aIService.js
+++ Backend/langlearnapi/services/aIService.js
@@ -1,6 +1,8 @@
-const {textGemini, textOpenAi}  = require("../data/textData");
-const {quizPrompt} = require("../prompts/prompts");
+const { textGemini, textOpenAi } = require("../data/textData");
+const { quizPrompt } = require("../prompts/prompts");
 exports.getQuiz = async function (language, difficulty) {
+  const prompt = quizPrompt(language, difficulty);
 
-    const prompt = 
-}
+  const response = await textOpenAi(prompt);
+  return response;
+};
