e01c082e3f8c073df26e4ed1654bff760e0a3650
Ananya <afmtrendaid@gmail.com>
2024-03-18 22:07:53 +0300

    User routes

--- Backend/langlearnapi/controllers/userController.js
+++ Backend/langlearnapi/controllers/userController.js
@@ -6,50 +6,47 @@ const {
   getUsers,
 } = require("../services/userService");
 
-
 exports.createUser = async function (req, res) {
-    try {
-        const user = await createUser(req.body);
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    };
+  try {
+    const user = await createUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.getUser = async function (req, res) {
-    try {
-        const user = await getUser(req.params.id);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    };
+  try {
+    const user = await getUser(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.getUsers = async function (req, res) {
-    try {
-        const users = await getUsers();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    };
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.updateUser = async function (req, res) {
-    try {
-        const user = await updateUser(req.params.id, req.body);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    }
+  try {
+    const user = await updateUser(req.params.id, req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.deleteUser = async function (req, res) {
-    try {
-        const user = await deleteUser(req.params.id);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    };
-
-
+  try {
+    const user = await deleteUser(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
