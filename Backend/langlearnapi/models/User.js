4a96b8ed0344c25abdc2442d9f6d9192d98c48f6
Ananya <afmtrendaid@gmail.com>
2024-03-18 18:44:56 +0300

    Model role changes

--- Backend/langlearnapi/models/User.js
+++ Backend/langlearnapi/models/User.js
@@ -38,6 +38,11 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+
+  role: {
+    type: String,
+    default: "user",
+  },
 });
 
 const User = mongoose.model("User", userSchema);
