52628b14c1b9bccb261d79ed4c3e532c8ac26ab8
Ananya <afmtrendaid@gmail.com>
2024-03-18 18:14:54 +0300

    Model payment

---
+++ Backend/langlearnapi/models/Payment.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    },
+    payment: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    status : {
+        type: String,
+        required: true,
+        default: false
+    }
+    });
