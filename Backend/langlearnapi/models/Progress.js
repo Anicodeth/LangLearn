410bac7be97bfb4c918d0b556e8f5910d03cf6e2
Ananya <afmtrendaid@gmail.com>
2024-03-18 18:07:09 +0300

    Model score

---
+++ Backend/langlearnapi/models/Progress.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const progressSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+  percentage: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Progress = mongoose.model("Progress", progressSchema);
+
+module.exports = Progress;
