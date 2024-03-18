67f0da1f8d259bceff3e2d8c81df9db258fdd605
Ananya <afmtrendaid@gmail.com>
2024-03-18 18:50:05 +0300

    formatting

--- Backend/langlearnapi/models/Language.js
+++ Backend/langlearnapi/models/Language.js
@@ -1,27 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const languageSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  languageImage: {
+    type: String,
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
     },
-    description: {
-        type: String,
-        required: true,
-    },
-    languageImage: {
-        type: String,
-    },
-    courses: [
-        {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course",
-        },
-    ],
-    }); 
+  ],
+});
 
 const Language = mongoose.model("Language", languageSchema);
 
 module.exports = Language;
-
-
