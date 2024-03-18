a55bc9a2b5a3eda489e56de23589e769e8b397f9
Ananya <afmtrendaid@gmail.com>
2024-03-18 18:12:00 +0300

    Model user

--- Backend/langlearnapi/models/User.js
+++ Backend/langlearnapi/models/User.js
@@ -34,6 +34,10 @@ const userSchema = new mongoose.Schema({
       ref: "Progress",
     },
   ],
+  coins: {
+    type: Number,
+    default: 0,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
