e4269ea6e587bd7efcc6e42f269c9ef9f53eaa83
Ananya <afmtrendaid@gmail.com>
2024-03-19 09:28:03 +0300

    ai

--- Backend/langlearnapi/controllers/aiController.js
+++ Backend/langlearnapi/controllers/aiController.js
@@ -1,2 +1,32 @@
+const { getQuiz, getChat, getTranslate } = require("../services/aiService");
 
-const {} = require("../services/aiService")
+exports.getQuiz = async function (req, res) {
+  try {
+    const response = await getQuiz(req.params.language, req.params.difficulty);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getChat = async function (req, res) {
+  try {
+    const response = await getChat(req.params.language, req.params.question);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getTranslate = async function (req, res) {
+  try {
+    const response = await getTranslate(
+      req.params.fromLanguage,
+      req.params.toLanguage,
+      req.params.text
+    );
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
