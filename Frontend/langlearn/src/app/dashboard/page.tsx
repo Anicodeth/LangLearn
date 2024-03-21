7a696972462498908fbf19f7302e5983b918ee29
Ananya <afmtrendaid@gmail.com>
2024-03-21 12:02:00 +0300

    sidebar shelf

--- Frontend/langlearn/src/app/dashboard/page.tsx
+++ Frontend/langlearn/src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center items-start p-10">
-      <div className = "w-full h-fit">
+      <div className="w-full h-fit">
         <Billboard />
       </div>
       <div></div>
@@ -16,7 +16,7 @@ function Billboard() {
   return (
     <div className="w-full h-60 flex flex-row justify-between items-start p-10 shadow-xl rounded-xmd bg-mainlighter">
       <div>
-        <h1 className = "text-2xl font-bold type-writer">Welcome to Lang Learn</h1>
+        <h1 className="text-2xl font-bold typewriter">Welcome to Lang Learn</h1>
         <p>- Epherata</p>
       </div>
       <div>
