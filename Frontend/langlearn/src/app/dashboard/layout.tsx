3c462977b3553b4c4d50d03b7ce7bc14f9e8a302
Ananya <afmtrendaid@gmail.com>
2024-03-21 21:56:21 +0300

    av

--- Frontend/langlearn/src/app/dashboard/layout.tsx
+++ Frontend/langlearn/src/app/dashboard/layout.tsx
@@ -14,6 +14,45 @@ import Image from "next/image";
 
 import Link from "next/link";
 import langLogo from "../../assets/langlearn2.png";
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
+
+const SmallAvatar = styled(Avatar)(({ theme }) => ({
+  width: 22,
+  height: 22,
+  border: `2px solid ${theme.palette.background.paper}`,
+}));
 interface ListItems {
   icon: any;
   text: string;
@@ -91,11 +130,32 @@ export default function DashboardLayout({
               height="100"
             />
           </div>
+          <div>
+ <BadgeAvatars />
+ 
+          </div>
+         
         </Card>
       </div>
       <div className="border-0 w-full h-full flex justify-center items-center ">
         {children}
       </div>
     </div>
+  );
+}
+
+
+ function BadgeAvatars() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <StyledBadge
+        overlap="circular"
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        variant="dot"
+      >
+        <Avatar alt="Remy Sharp"  />
+      </StyledBadge>
+      
+    </Stack>
   );
 }
