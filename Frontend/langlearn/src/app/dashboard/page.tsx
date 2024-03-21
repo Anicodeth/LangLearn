f439e26b6f51facc7884ba4cac7494dc40f78059
Ananya <afmtrendaid@gmail.com>
2024-03-21 11:51:10 +0300

    sidebar shelf

--- Frontend/langlearn/src/app/dashboard/page.tsx
+++ Frontend/langlearn/src/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function Dashboard() {
-  return <>HI</>;
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex justify-center items-start p-10">
+      <div className = "w-full h-fit">
+        <Billboard />
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+function Billboard() {
+  return (
+    <div className="w-full h-60 flex flex-col justify-start items-start border-2 p-10 shadow-m rounded-xmd">
+      <div>
+        <h1>Welcome to Lang Learn</h1>
+        <p>- Epherata</p>
+      </div>
+      <
+    </div>
+  );
 }
--- Frontend/langlearn/tailwind.config.ts
+++ Frontend/langlearn/tailwind.config.ts
@@ -68,6 +68,7 @@ const config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+        xmd: "1.5rem"
       },
       keyframes: {
         "accordion-down": {
