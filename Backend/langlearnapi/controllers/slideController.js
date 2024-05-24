8708f11cc0ad195d60f0c54b38b2e6426caca0a4
Ananya <afmtrendaid@gmail.com>
2024-05-24 19:27:51 +0300

    Slides

--- Backend/langlearnapi/controllers/slideController.js
+++ Backend/langlearnapi/controllers/slideController.js
@@ -1,51 +1,46 @@
-const Slide = require("../models/Slide");
+const slideController = require("../controllers/slideController");
 
-exports.createSlide = async function (slide) {
+exports.createSlide = async function (req, res) {
   try {
-    const newSlide = await Slide(slide);
-    await newSlide.save();
-    return newSlide;
+    const slide = await slideController.createSlide(req.body);
+    return res.status(201).json(slide);
   } catch (error) {
-    throw error;
+    return res.status(400).json(error.message);
   }
 };
 
-exports.getSlide = async function (id) {
+exports.getSlide = async function (req, res) {
   try {
-    const slide = await Slide.findById(id);
-    return slide;
+    const slide = await slideController.getSlide(req.params.id);
+    return res.status(200).json(slide);
   } catch (error) {
-    throw error;
+    return res.status(404).json(error.message);
   }
 };
 
-exports.getSlides = async function () {
+exports.getSlides = async function (req, res) {
   try {
-    const slides = await Slide.find();
-    return slides;
+    const slides = await slideController.getSlides();
+    return res.status(200).json(slides);
   } catch (error) {
-    throw error;
+    return res.status(404).json(error.message);
   }
 };
 
-exports.updateSlide = async function (id, slide) {
+exports.updateSlide = async function (req, res) {
   try {
-    const updatedSlide = await Slide.findByIdAndUpdate(id, slide, {
-      new: true,
-    });
-    await updatedSlide.save();
-    return updatedSlide;
+    const slide = await slideController.updateSlide(req.params.id, req.body);
+    return res.status(200).json(slide);
   } catch (error) {
-    throw error;
+    return res.status(400).json(error.message);
   }
 };
 
-exports.deleteSlide = async function (id) {
+exports.deleteSlide = async function (req, res) {
   try {
-    const slide = await Slide.findByIdAndDelete(id);
-    await slide.save();
-    return slide;
+    const slide = await slideController.deleteSlide(req.params.id);
+    return res.status(200).json(slide);
   } catch (error) {
-    throw error;
+    return res.status(404).json(error.message);
   }
 };
