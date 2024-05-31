11bf56612676b49b2baac6165e0f84c9941c8203
Ananya <afmtrendaid@gmail.com>
2024-05-31 12:58:39 +0300

    gemini

--- Backend/langlearnapi/services/aiService.js
+++ Backend/langlearnapi/services/aiService.js
@@ -1,23 +1,27 @@
 const { textGemini, textOpenAi } = require("../data/textData");
-const { quizPrompt, chatPrompt, translatePrompt } = require("../prompts/prompts");
+const {
+  quizPrompt,
+  chatPrompt,
+  translatePrompt,
+} = require("../prompts/prompts");
 
 exports.getQuiz = async function (language, difficulty) {
   const prompt = quizPrompt(language, difficulty);
 
-  const response = await textOpenAi(prompt);
+  const response = await textGemini(prompt);
   return response;
 };
 
 exports.getChat = async function (language, question) {
   const prompt = chatPrompt(language, question);
 
-  const response = await textOpenAi(prompt);
+  const response = await textGemini(prompt);
   return response;
 };
 
 exports.getTranslate = async function (fromLanguage, toLanguage, text) {
   const prompt = translatePrompt(fromLanguage, toLanguage, text);
 
-  const response = await textOpenAi(prompt);
+  const response = await textGemini(prompt);
   return response;
 };
