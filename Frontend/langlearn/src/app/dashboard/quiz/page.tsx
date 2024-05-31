17c27ae457939619f923e2644bfb37eb225c03df
Ananya <afmtrendaid@gmail.com>
2024-05-31 16:13:03 +0300

    services

--- Frontend/langlearn/src/app/dashboard/quiz/page.tsx
+++ Frontend/langlearn/src/app/dashboard/quiz/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { useQuery } from "react-query";
 import {
   Carousel,
   CarouselContent,
@@ -8,7 +9,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import * as React from "react";
+
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const questions = [
   {
@@ -29,7 +49,87 @@ const questions = [
   // Add more questions here
 ];
 
-export default function Quiz() {}
+const languages = [
+  "Amharic",
+  "English",
+  "Spanish",
+  "French",
+  "German",
+  "Japanese",
+  "Korean",
+  "Chinese",
+  "Russian",
+  "Italian",
+  "Portuguese",
+  "Arabic",
+  "Hindi",
+  "Turkish",
+];
+
+const dificulty = ["Easy", "Medium", "Hard"];
+
+export default function Quiz() {
+  const [language, setLanguage] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  
+  //use query 
+  const { data, isLoading,  error } = useQuery("quiz", async () => {});
+
+  function handleStart() {
+    console.log("Start");
+    console.log(language, difficulty);
+  }
+
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Quiz Time!</CardTitle>
+        <CardDescription>
+          Choose a language and difficulty to learn and test your knowledge.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="language">Language</Label>
+              <Select value={language} onValueChange={setLanguage}>
+                <SelectTrigger id="language">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  {languages.map((lang: string, index: number) => (
+                    <SelectItem key={index} value={lang}>
+                      {lang}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>{" "}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="difficulty">Difficulty</Label>
+              <Select value={difficulty} onValueChange={setDifficulty}>
+                <SelectTrigger id="difficulty">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  {dificulty.map((diff: string, index: number) => (
+                    <SelectItem key={index} value={diff}>
+                      {diff}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button onClick={handleStart}>Start</Button>
+      </CardFooter>
+    </Card>
+  );
+}
 
 function Questions() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
