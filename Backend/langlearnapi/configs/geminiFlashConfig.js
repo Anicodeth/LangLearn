52d4db0176a9af769b3ad085cba61aa82610782a
Ananya <afmtrendaid@gmail.com>
2024-06-07 19:39:20 +0300

    all

---
+++ Backend/langlearnapi/configs/geminiFlashConfig.js
@@ -0,0 +1,40 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI("AIzaSyC0MN0QlTwAjIZ9_hCv_s6NIPlZO6Dcfms");
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text();
+}
+
+module.exports = { run };
--- Backend/langlearnapi/services/aiService.js
+++ Backend/langlearnapi/services/aiService.js
@@ -5,23 +5,25 @@ const {
   translatePrompt,
 } = require("../prompts/prompts");
 
+const { run } = require("../configs/geminiFlashConfig");
+
 exports.getQuiz = async function (language, difficulty) {
   const prompt = quizPrompt(language, difficulty);
 
-  const response = await textGemini(prompt);
+  const response = await run(prompt);
   return response;
 };
 
 exports.getChat = async function (language, question) {
   const prompt = chatPrompt(language, question);
 
-  const response = await textGemini(prompt);
+  const response = await run(prompt);
   return response;
 };
 
 exports.getTranslate = async function (fromLanguage, toLanguage, text) {
   const prompt = translatePrompt(fromLanguage, toLanguage, text);
 
-  const response = await textGemini(prompt);
+  const response = await run(prompt);
   return response;
 };
