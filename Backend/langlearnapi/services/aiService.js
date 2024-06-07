ebe553087b984c81ebe21c184d99d16ab070146f
Ananya <afmtrendaid@gmail.com>
2024-06-07 17:31:16 +0300

    open ai

--- Backend/langlearnapi/services/aiService.js
+++ Backend/langlearnapi/services/aiService.js
@@ -8,20 +8,20 @@ const {
 exports.getQuiz = async function (language, difficulty) {
   const prompt = quizPrompt(language, difficulty);
 
-  const response = await textOpenAi(prompt);
+  const response = await textGemini(prompt);
   return response;
 };
 
 exports.getChat = async function (language, question) {
   const prompt = chatPrompt(language, question);
 
-  const response = await textOpenAi(prompt);
+  const response = await textGemini(prompt);
   return response;
 };
 
 exports.getTranslate = async function (fromLanguage, toLanguage, text) {
   const prompt = translatePrompt(fromLanguage, toLanguage, text);
 
-  const response = await textOpenAi(prompt);
+  const response = await textGemini(prompt);
   return response;
 };
