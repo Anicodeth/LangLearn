fe3ed8d61840fbb71dbe648792b2967a7d6df124
Ananya <afmtrendaid@gmail.com>
2024-06-07 12:05:57 +0300

    getChat

--- Frontend/langlearn/src/service/aiService.ts
+++ Frontend/langlearn/src/service/aiService.ts
@@ -1,27 +1,21 @@
-
 import axios from "axios";
 
 const apiUrl = "https://lang-learn-api.vercel.app/api/v1/ai";
 
-export const getQuiz = async (languge:string, difficulty:string) => {
-    try {
-        const response = await axios.get(`${apiUrl}/quiz/${languge}/${difficulty}`);
-        return JSON.parse(response.data);
-    } catch (error) {
-        throw error;
-    }
-    }
+export const getQuiz = async (languge: string, difficulty: string) => {
+  try {
+    const response = await axios.get(`${apiUrl}/quiz/${languge}/${difficulty}`);
+    return JSON.parse(response.data);
+  } catch (error) {
+    throw error;
+  }
+};
 
-
-export const getChat = async (message:string, language:string) => {
-    try {
-        const response = await axios.post(`${apiUrl}/chat`, {
-            message: message,
-            language: language
-        });
-        return response.data.reply;
-    } catch (error) {
-        throw error;
-    }
-}
-
+export const getChat = async (message: string, language: string) => {
+  try {
+    const response = await axios.get(`${apiUrl}/chat/${language}/${message}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
