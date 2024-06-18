9fdcd7f93e20e079961da31e49d950f26ae2213e
Ananya <afmtrendaid@gmail.com>
2024-06-19 00:13:49 +0300

    admin main features

--- Frontend/langlearn/src/app/dashboard/addcourse/[courseid]/page.tsx
+++ Frontend/langlearn/src/app/dashboard/addcourse/[courseid]/page.tsx
@@ -5,7 +5,7 @@ import {
   removeSlideFromCourse,
 } from "@/service/courseService";
 import { toast } from "sonner";
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import {
   Dialog,
   DialogTrigger,
@@ -52,7 +52,7 @@ export default function CourseSlidesPage({
       <AddSlideCourseDialog courseId={params.courseid} />
       <div className="flex flex-wrap gap-4">
         {slidesData.map((slide: any) => (
-          <Slide key={slide._id} slide={slide} />
+          <Slide key={slide._id} slide={slide}  courseId={params.courseid}/>
         ))}
       </div>
     </div>
@@ -285,7 +285,7 @@ function AddSlideCourseDialog({ courseId }: { courseId: string }) {
   );
 }
 
-function Slide({ slide }: { slide: any }) {
+function Slide({ slide, courseId }: { slide: any, courseId: string}) {
   return (
     <div>
       <div
@@ -313,15 +313,18 @@ function Slide({ slide }: { slide: any }) {
           </p>
         )}
       </div>
-      <DeleteSlideDialog slideId={slide._id} />
-      <EditSlideDialog slide={slide} />
+      <div className = "w-full flex justify-stretch gap-4 ">
+        <DeleteSlideDialog  slideId={slide._id} courseId={courseId} />
+        <EditSlideDialog slide={slide} />
+      </div>
     </div>
   );
 }
 
-function DeleteSlideDialog({ slideId }: { slideId: string }) {
-  const deleteSlideMutation = useMutation(() => deleteSlide(slideId), {
+function DeleteSlideDialog({ slideId, courseId }: { slideId: string, courseId: string}) {
+  const deleteSlideMutation = useMutation(() => removeSlideFromCourse(courseId, slideId), {
     onSuccess: () => {
+      queryClient.invalidateQueries("courseSlides");
       toast.success("Slide deleted successfully");
     },
     onError: () => {
@@ -333,10 +336,12 @@ function DeleteSlideDialog({ slideId }: { slideId: string }) {
     await deleteSlideMutation.mutateAsync();
   };
 
+  const queryClient = useQueryClient();
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button>Delete</Button>
+        <Button className="w-full" variant="destructive">Delete</Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
@@ -373,6 +378,8 @@ function EditSlideDialog({ slide }: { slide: any }) {
   const [color, setColor] = useState(slide.color);
   const [font, setFont] = useState(slide.font);
 
+  const queryClient = useQueryClient();
+
   const editSlideMutation = useMutation(
     () =>
       updateSlide(slide._id, {
@@ -391,6 +398,7 @@ function EditSlideDialog({ slide }: { slide: any }) {
       }),
     {
       onSuccess: () => {
+        queryClient.invalidateQueries("courseSlides");
         toast.success("Slide updated successfully");
       },
       onError: () => {
@@ -406,152 +414,203 @@ function EditSlideDialog({ slide }: { slide: any }) {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button>Edit</Button>
+        <Button className="w-full">Edit</Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Edit Slide</DialogTitle>
           <DialogDescription>Edit the slide below</DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Title
-            </Label>
-            <Input
-              id="Slide Title"
-              value={slideTitle}
-              onChange={(e) => setSlideTitle(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+        <div className="flex flex-row justify-between gap-4">
+          <div className="grid gap-4 py-4 h-72 overflow-scroll">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Title
+              </Label>
+              <Input
+                id="Slide Title"
+                value={slideTitle}
+                onChange={(e) => setSlideTitle(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Image
-            </Label>
-            <Input
-              id="Slide Image"
-              value={slideImage}
-              onChange={(e) => setSlideImage(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Image
+              </Label>
+              <Input
+                id="Slide Image"
+                value={slideImage}
+                onChange={(e) => setSlideImage(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Text
-            </Label>
-            <Input
-              id="Slide Text"
-              value={slideText}
-              onChange={(e) => setSlideText(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Text
+              </Label>
+              <Input
+                id="Slide Text"
+                value={slideText}
+                onChange={(e) => setSlideText(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Audio
-            </Label>
-            <Input
-              id="Slide Audio"
-              value={slideAudio}
-              onChange={(e) => setSlideAudio(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Audio
+              </Label>
+              <Input
+                id="Slide Audio"
+                value={slideAudio}
+                onChange={(e) => setSlideAudio(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Video
-            </Label>
-            <Input
-              id="Slide Video"
-              value={slideVideo}
-              onChange={(e) => setSlideVideo(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Video
+              </Label>
+              <Input
+                id="Slide Video"
+                value={slideVideo}
+                onChange={(e) => setSlideVideo(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Question
-            </Label>
-            <Input
-              id="Slide Question"
-              value={slideQuestion}
-              onChange={(e) => setSlideQuestion(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Question
+              </Label>
+              <Input
+                id="Slide Question"
+                value={slideQuestion}
+                onChange={(e) => setSlideQuestion(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Choices
-            </Label>
-            <Input
-              id="Slide Choices"
-              value={slideChoices}
-              onChange={(e) => setSlideChoices(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Choices
+              </Label>
+              <Input
+                id="Slide Choices"
+                value={slideChoices}
+                onChange={(e) => setSlideChoices(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Correct Answer
-            </Label>
-            <Input
-              id="Slide Correct Answer"
-              value={slideCorrectAnswer}
-              onChange={(e) => setSlideCorrectAnswer(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Slide Correct Answer
+              </Label>
+              <Input
+                id="Slide Correct Answer"
+                value={slideCorrectAnswer}
+                onChange={(e) => setSlideCorrectAnswer(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Slide Explanation
-            </Label>
-            <Input
-              id="Slide Explanation"
-              value={slideExplanation}
-              onChange={(e) => setSlideExplanation(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Color
+              </Label>
+              <Input
+                id="Color"
+                type="color"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+                className="col-span-3 w-60"
+              />
+            </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Color
-            </Label>
-            <Input
-              id="Color"
-              value={color}
-              onChange={(e) => setColor(e.target.value)}
-              className="col-span-3 w-60"
-            />
-          </div>
-
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Font
-            </Label>
-            <Input
-              id="Font"
-              value={font}
-              onChange={(e) => setFont(e.target.value)}
-              className="col-span-3 w-60"
-            />
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="font" className="text-right">
+                Font
+              </label>
+              <select
+                id="font"
+                value={font}
+                onChange={(e)=>setFont(e.target.value)}
+                className="col-span-3 w-60 p-2 border border-gray-300 rounded-md shadow-sm"
+              >
+                <option value="">Select a font</option>
+                <option value="Arial">Arial</option>
+                <option value="Verdana">Verdana</option>
+                <option value="Times New Roman">Times New Roman</option>
+                <option value="Courier New">Courier New</option>
+                <option value="Georgia">Georgia</option>
+              </select>
+            </div>
           </div>
         </div>
         <DialogFooter>
-          <Button onClick={handleSlideEdit}>
-            {editSlideMutation.isLoading ? "Editing..." : "Edit"}
-          </Button>
+          <div className="flex w-full h-40 overflow-scroll flex-col items-center">
+            <Button className="w-full" onClick={handleSlideEdit}>
+              {editSlideMutation.isLoading ? "Saving..." : "Save"}
+            </Button>
+            <PreviewSlides
+              slide={{
+                slideTitle,
+                slideImage,
+                slideText,
+                slideAudio,
+                slideNewWord,
+                slideVideo,
+                slideQuestion,
+                slideChoices,
+                slideCorrectAnswer,
+                slideExplanation,
+                color,
+                font,
+              }}
+            />
+          </div>
         </DialogFooter>
       </DialogContent>
     </Dialog>
+  );
+}
+
+
+
+function PreviewSlides({ slide }: { slide: any }) {
+  return (
+    <div>
+      <div
+        className="border w-80 h-96 p-4 rounded-md my-2"
+        style={{
+          fontFamily: `${slide.font}`,
+          backgroundColor: `${slide.color}`,
+        }}
+      >
+        <h1 className="w-full text-center">{slide.slideTitle}</h1>
+        {slide.slideImage && (
+          <img
+            src={slide.slideImage}
+            alt={slide.slideTitle}
+            className="w-full"
+          />
+        )}
+        <p>{slide.slideText}</p>
+        {slide.slideAudio && <audio src={slide.slideAudio} controls></audio>}
+        <p>{slide.slideNewWord}</p>
+        {slide.slideAudio && <video src={slide.slideVideo} controls></video>}
+        {slide.slideQuestion && (
+          <p className="bg-black text-white font-bold text-l p-5 rounded">
+            {slide.slideQuestion}
+          </p>
+        )}
+      </div>
+
+    </div>
   );
 }
