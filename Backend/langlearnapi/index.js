04363facc2885025817eeaa20e291bd5610e526a
Ananya <afmtrendaid@gmail.com>
2024-07-02 17:52:17 +0300

    coins

--- Backend/langlearnapi/index.js
+++ Backend/langlearnapi/index.js
@@ -32,7 +32,7 @@ const swaggerSpec = swaggerJsdoc({
       description: "Langlearn 1.0 API documentation",
     },
   },
-  apis: ["./routes/*.js"], 
+  apis: ["./routes/*.js"],
 });
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
@@ -54,6 +54,30 @@ app.use(`/api/${VERSION}/languages`, languageRoutes);
 app.use(`/api/${VERSION}/courses`, courseRoutes);
 app.use(`/api/${VERSION}/slides`, slideRoutes);
 app.use(`/api/${VERSION}/scores`, scoreRoutes);
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.listen(4000, () => {
   console.log("Connected");
