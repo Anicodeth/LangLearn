182e04098013cd4f6fbf5b73f921275c7ddc309a
Ananya <afmtrendaid@gmail.com>
2024-07-02 17:33:32 +0300

    coins

--- Frontend/langlearn/src/app/dashboard/learn/page.tsx
+++ Frontend/langlearn/src/app/dashboard/learn/page.tsx
@@ -21,33 +21,36 @@ export default function Learn() {
   const filteredData = data.filter((language: any) => language !== null);
   return (
     <div>
-    <h1 className = "p-4 font-bold text-xl">Languages</h1>
-    <div className="p-4 grid lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 gap-4 h-full">
-
-      {filteredData.map((language: any) => (
-        <Card key={language._id} className="flex justify-between flex-col">
-          <CardHeader className="">
-            <img src="https://img.icons8.com/?size=100&id=6MP1kA74ozKg&format=png&color=000000" />
-            <CardTitle>{language.name}</CardTitle>
-            <CardDescription>{language.description}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex justify-between">
-              <Button
-                onClick={() => router.push(`/dashboard/learn/${language._id}`)}
-              >
-                See Courses
-              </Button>
-              <Button
-                onClick={() => router.push(`/dashboard/chat/${language._id}`)}
-              >
-                Chat
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+      <h1 className="p-4 font-bold text-xl">Languages</h1>
+      <div className="p-4 grid lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 gap-4 h-full">
+        {filteredData.map((language: any) => (
+          <Card key={language._id} className="flex justify-between flex-col">
+            <CardHeader className="">
+              <img src="https://img.icons8.com/?size=100&id=6MP1kA74ozKg&format=png&color=000000" />
+              <CardTitle>{language.name}</CardTitle>
+              <CardDescription>{language.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex justify-between">
+                <Button
+                  onClick={() =>
+                    router.push(`/dashboard/learn/${language._id}`)
+                  }
+                >
+                  See Courses
+                </Button>
+                <Button
+                  onClick={() =>
+                    router.push(`/dashboard/chat/${language.name}`)
+                  }
+                >
+                  Chat
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
